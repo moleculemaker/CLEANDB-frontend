c508fa05f2247c8313aed1375580ab791b4020e8
matthewberry <github@mjberry.com>
2025-06-28 20:44:43 -0500

    added environments

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: false,
-  apiBaseUrl: 'http://mmli.fastapi.localhost' // Base URL for local environment
+  apiBaseUrl: 'http://mmli.fastapi.localhost', // Base URL for local environment
+  cleandbBaseUrl: 'https://fastapi.cleandb.mmli1.ncsa.illinois.edu', // Base URL for CleanDB API
 };
